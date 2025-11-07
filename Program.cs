eb8aa6f337f6daf07b57f71c254d82f082cddc75
dante <dante.tarragau@gmail.com>
2025-11-07 02:54:49 -0500

    Agregar carga de variables de entorno y ajustar cadena de conexión en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
+using System;
 using WorkersManagementApi.Data;
 using WorkersManagementApi.Services;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<WorkerService>();
 
